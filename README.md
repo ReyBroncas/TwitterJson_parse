ee89365102bc32a0587723c49a80001b99c70d94
ReyBroncas <ReyBroncas@github.com>
2020-02-24 01:06:16 +0200

    Added Readme + fix

---
+++ README.md
@@ -0,0 +1,37 @@
+### Getting started
+'''
+python3 twitter.py
+Enter a search query: python
+'''
+### Example output
+'''
+─> RT @realpython: 🐍📺 The VS Code Python Extension [Video]
+#python https://t.co/wmzKYTg8hX
+
+├── [0|str] ──── created_at
+├── [1|int] ──── id
+├── [2|str] ──── id_str
+├── [3|str] ──── text
+├── [4|bool] ──── truncated
+├── [5|dict] ──── entities
+├── [6|dict] ──── metadata
+├── [7|str] ──── source
+├── [8|NoneType] ──── in_reply_to_status_id
+├── [9|NoneType] ──── in_reply_to_status_id_str
+├── [10|NoneType] ──── in_reply_to_user_id
+├── [11|NoneType] ──── in_reply_to_user_id_str
+├── [12|NoneType] ──── in_reply_to_screen_name
+├── [13|dict] ──── user
+├── [14|NoneType] ──── geo
+├── [15|NoneType] ──── coordinates
+├── [16|NoneType] ──── place
+├── [17|NoneType] ──── contributors
+├── [18|dict] ──── retweeted_status
+├── [19|bool] ──── is_quote_status
+├── [20|int] ──── retweet_count
+├── [21|int] ──── favorite_count
+├── [22|bool] ──── favorited
+├── [23|bool] ──── retweeted
+├── [24|bool] ──── possibly_sensitive
+├── [25|str] ──── lang
+'''
--- essential.py
+++ essential.py
@@ -10,7 +10,8 @@ def search_tweet(q, api_reference):
         url = twhandle.augment(api_reference, {'q': q})
         data = urllib.request.urlopen(url).read()
         js = json.loads(data)
-        os.makedirs(f'cache')
+        if not os.path.isdir('cache'):
+            os.makedirs(f'cache')
         with open(f'cache/{q}.json', 'w') as outfile:
             json.dump(js, outfile)
         return js
@@ -20,31 +21,31 @@ def search_tweet(q, api_reference):
         return js
 
 
-def json_navigate(obj_elem):
+def json_navigate(obj_elm):
     while True:
-        if isinstance(obj_elem, (list, dict, tuple)):
-            if isinstance(obj_elem, (list, tuple)):
-                ind_l = list(range(len(obj_elem)))
+        if isinstance(obj_elm, (list, dict, tuple)):
+            if isinstance(obj_elm, (list, tuple)):
+                ind_l = list(range(len(obj_elm)))
             else:
-                ind_l = list(obj_elem.keys())
-        elif obj_elem is None:
+                ind_l = list(obj_elm.keys())
+        elif obj_elm is None:
             print('\n\n──> Nothing here...\n')
             return
         else:
-            print(f'\n\n──> {obj_elem}\n')
+            print(f'\n\n──> {obj_elm}\n')
             return
-        for i, each in enumerate(obj_elem):
+        for i, each in enumerate(obj_elm):
             if not isinstance(each, (list, tuple, dict)):
-                print(f'├── [{i}|{obj_elem[ind_l[i]].__class__.__name__}] ──── {each}')
+                print(f'├── [{i}|{obj_elm[ind_l[i]].__class__.__name__}] ──── {each}')
             else:
-                print(f'├── [{i}|{obj_elem[ind_l[i]].__class__.__name__}] ──── /.')
-        value = input('\nEnter an index of element or press "b" to move back: ')
+                print(f'├── [{i}|{obj_elm[ind_l[i]].__class__.__name__}] ──── /.')
+        value = input('\nEnter an index of element or press "h" to move back: ')
         os.system('cls' if os.name == 'nt' else 'clear')
         if value == 'h':
             return
         else:
             value = int(value)
-        json_navigate(obj_elem[ind_l[value]])
+        json_navigate(obj_elm[ind_l[value]])
 
 
 if __name__ == '__main__':
